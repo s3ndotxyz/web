4691d59aa0db32de6be65d31f03893bf27029412
hey-ewan <ewanretorokugbe@gmail.com>
2025-03-24 06:04:15 +0100

    cleaned and formatted

--- apps/cloud/utils/para.ts
+++
@@ -1,4 +0,0 @@
-// import { Environment, ParaWeb } from "@getpara/web-sdk";
-
-// const PARA_API_KEY = import.meta.env.PARA_API_KEY;
-// export const para = new ParaWeb(Environment.BETA, PARA_API_KEY);
